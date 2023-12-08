b439468acd8d338c023148eddcfc6409685a3ac6
Haroon Yasin <haroon.yasin95@gmail.com>
2023-12-08 12:44:53 +0000

    done

--- primes.py
+++ primes.py
@@ -2,5 +2,27 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+
+    if number_of_primes < 1:
+        raise ValueError("Only positive integets allowed")
+
+    primes = []
+
+    count = 0
+    vals = 2
+
+    while (count < number_of_primes):
+        if isPrime(vals):
+            primes.append(vals)
+            count += 1
+        vals += 1
+
+    return primes
+
+def isPrime(value):
+    flag = True
+    for i in range(2,value):
+        if (value % i) == 0:
+            flag = False
+            break
+    return flag
